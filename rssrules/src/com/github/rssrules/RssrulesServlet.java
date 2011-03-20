1e3d3b701b6b88fb71a705c0654b67279e873914
agea <agea@drwolf.it>
2011-03-20 01:36:22 +0100

    First commit, skeleton google app engine project

---
+++ rssrules/src/com/github/rssrules/RssrulesServlet.java
@@ -0,0 +1,13 @@
+package com.github.rssrules;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class RssrulesServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}

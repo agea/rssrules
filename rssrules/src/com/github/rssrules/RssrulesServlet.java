7183ba04c8404dfc628de803fedb73e1dc935ef1
agea <agea@drwolf.it>
2011-04-05 09:14:22 +0200

    working on server side logic

--- rssrules/src/com/github/rssrules/RssrulesServlet.java
+++ rssrules/src/com/github/rssrules/RssrulesServlet.java
@@ -6,9 +6,10 @@ import java.io.StringWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,10 +25,11 @@ import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.SyndFeedOutput;
 import com.sun.syndication.io.XmlReader;
 
-@SuppressWarnings("serial")
 public class RssrulesServlet extends HttpServlet {
 
-	@SuppressWarnings("rawtypes")
+	private static final long serialVersionUID = -8848841096627270590L;
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		ArrayList<Exception> errors = new ArrayList<Exception>();
@@ -38,24 +40,74 @@ public class RssrulesServlet extends HttpServlet {
 			URL url = new URL(req.getParameter("u"));
 			boolean and = req.getParameter("l").equals("and");
 
-			HashMap<String, String> rules = new HashMap<String, String>();
-			Set s = req.getParameterMap().entrySet();
-
-			String[] titleContains = req.getParameterValues("tc");
-			String[] notTitleContains = req.getParameterValues("ntc");
-			String[] titleStarts = req.getParameterValues("ts");
-			String[] notTitleStarts = req.getParameterValues("nts");
-
-			SyndFeedInput input = new SyndFeedInput();
-
-			SyndFeed feed = input.build(new XmlReader(url));
+			SyndFeed feed = new SyndFeedInput().build(new XmlReader(url));
 			Iterator i = feed.getEntries().iterator();
-			boolean rem = false;
+
 			while (i.hasNext()) {
-				i.next();
-				rem = !rem;
-				if (rem) {
-					i.remove();
+				SyndEntry post = (SyndEntry) i.next();
+				for (Entry<String, String[]> entry : (Set<Entry<String, String[]>>) req
+						.getParameterMap().entrySet()) {
+					String k = entry.getKey();
+					if (k.equals("u") || k.equals("l")) {
+						continue;
+					}
+					String[] vs = entry.getValue();
+					for (String v : vs) {
+						boolean neg = k.startsWith("n");
+						if (neg) {
+							k = k.substring(1);
+						}
+						String[] tbv = new String[0];
+						String part = k.substring(0, 1);
+						if (part.equals("p")) {
+							tbv = new String[] { post.getCategories()
+									.toString()
+									+ post.getTitle()
+									+ post.getDescription() + post.getAuthor() };
+						} else if (part.equals("t")) {
+							tbv = new String[] { post.getTitle() };
+						} else if (part.equals("c")) {
+							Object[] obj = post.getCategories().toArray();
+							tbv = new String[obj.length];
+							for (int j = 0; j < obj.length; j++) {
+								tbv[j] = obj[j].toString();
+							}
+						} else if (part.equals("a")) {
+							tbv = new String[] { post.getAuthor() };
+						} else if (part.equals("b")) {
+							tbv = new String[] { post.getDescription()
+									.getValue() };
+						}
+						Boolean m = false;
+						String r = k.substring(2, 3);
+						if (r.equals("c")) {
+							for (String tbvs : tbv) {
+								m = tbvs.toLowerCase()
+										.contains(v.toLowerCase()) || m;
+							}
+						}
+						if (r.equals("s")) {
+							for (String tbvs : tbv) {
+								m = tbvs.toLowerCase().startsWith(
+										v.toLowerCase())
+										|| m;
+							}
+						}
+						if (r.equals("e")) {
+							for (String tbvs : tbv) {
+								m = tbvs.toLowerCase()
+										.endsWith(v.toLowerCase()) || m;
+							}
+						}
+						if (r.equals("m")) {
+							Pattern p = Pattern.compile(v);
+							for (String tbvs : tbv) {
+								m = p.matcher(tbvs).matches() || m;
+							}
+						}
+						// if !and and !neg if m remove and break
+					}
+
 				}
 			}
 
